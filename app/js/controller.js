db6c34e804e8d1a9e51b84de73219d998bfc73d3
sylvaindev <sylvain.martin@epitech.eu>
2015-07-15 09:34:05 +0200

    - Ajout du changement de prix en fonction de la quantité d'articles sélectionnés. - Ajout du changement de nombre d'articles en dynamique.

--- app/js/controller.js
+++ app/js/controller.js
@@ -5,19 +5,38 @@ angular.module('Commande', [])
     .controller('commandeController', ['$scope', function($scope) {
 
         $scope.articles = [
-                { id: 1, reference: 123, titre: "MSI GTX 980ti", prixUnitaire: "666,63 €", montantHT: 666.63, montantTTC: 799.95 },
-                { id: 2, reference: 456, titre: "Intel Core i7-4770K", prixUnitaire: "324,96 €", montantHT: 324.96, montantTTC: 389.95 },
-                { id: 3, reference: 789, titre: "ASUS MAXIMUS VII RANGER", prixUnitaire: "134,96 €", montantHT: 134.96, montantTTC: 161.95 }
-        ]
+                { id: 1, reference: 123, titre: "MSI GTX 980ti", prixUnitaire: 666.63, quantite: 0, montantHT: 666.63, montantTTC: 799.95 },
+                { id: 2, reference: 456, titre: "Intel Core i7-4770K", prixUnitaire: 324.96, quantite: 0, montantHT: 324.96, montantTTC: 389.95 },
+                { id: 3, reference: 789, titre: "ASUS MAXIMUS VII RANGER", prixUnitaire: 134.96, quantite: 0, montantHT: 134.96, montantTTC: 161.95 }
+        ];
 
-        $scope.NombreArticle = "Votre panier contient " + $scope.articles.length + " articles";
 
-        $scope.TotalTTC = 0;
-        $scope.TotalHT = 0;
 
-        angular.forEach($scope.articles, function(article){
-                $scope.TotalTTC += article.montantTTC;
-                $scope.TotalHT += article.montantHT;
-        });
+        $scope.PrixTotalTTC = function() {
+            var resultTTC = 0;
+
+            angular.forEach($scope.articles, function (article) {
+                resultTTC += article.montantTTC * article.quantite;
+            });
+            return resultTTC;
+        }
+
+        $scope.PrixTotalHT = function() {
+            var resultHT = 0;
+
+            angular.forEach($scope.articles, function (article) {
+                resultHT += article.montantHT * article.quantite;
+            });
+            return resultHT;
+        }
+
+        $scope.NombreArticle = function() {
+            var resultArticle = 0;
+
+            angular.forEach($scope.articles, function(article){
+                resultArticle += article.quantite;
+            })
+            return resultArticle;
+        }
 
     }]);
